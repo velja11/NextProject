9d44d5ba4d20f14c9b478e7875a75a040b8ead96
Velibor Solujić <velibor.solujic@rgz.local>
2022-05-31 14:56:26 +0200

    Adding custom classes

--- components/card.tsx
+++ components/card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Card:React.FC<{title:string, name:string}> = (props) => {
-   return ( <div className='flex flex-col  justify-center items-center w-2/12	bg-white border-4 border-blue-400 mt-5 p-4 rounded-xl hover:bg-blue-300 hover:text-white transition easy-out duration-500 md:ml-4'>
+   return ( <div className='flex flex-col  justify-center items-center	bg-white border-4 border-blue-400 mt-5 p-4 rounded-xl hover:bg-blue-300 hover:text-white transition easy-out duration-500 md:ml-4 md:w-2/12'>
       <h1 className="text-4xl font-bold">{props.title}</h1>
       <p className='mt-5'>{props.name}</p> 
       </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,7 +12,7 @@ const Navbar:React.FC<{children:React.ReactNode}> = (props) => {
             <li className='ml-6'>Page 3</li>
           </ul>
         </nav> */}
-        <Link href="/"><h1 className='ml-4 font-bold lowercase text-3xl md:uppercase cursor-pointer hover:text-white'>Next project</h1></Link>
+        <Link href="/"><h1 className='logo'>Next project</h1></Link>
         <main className='mr-6'>
           <div>
             <Link href="/login"><a className='bg-white rounded-md pl-4 pt-1 pb-1 pr-4 border-2 border-blue-800 hover:bg-blue-400 hover:text-white transition easy-in duration-500'>Login</a></Link>
